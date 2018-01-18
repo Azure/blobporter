9c4a6a10cbd76247415045d69bb3641d2f4fee83
Jesus Aguilar <3589801+giventocode@users.noreply.github.com>
2018-01-18 02:52:24 -0500

    -  readme update - sync and lock fixes for the file handel pool

--- targets/multifile.go
+++ targets/multifile.go
@@ -8,8 +8,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/Azure/blobporter/util"
-
 	"github.com/Azure/blobporter/pipeline"
 )
 
@@ -116,12 +114,15 @@ func (h *fileHandleManager) getHandle(path string) (*os.File, error) {
 
 	if !isInit {
 		fh, err = h.initFile(path)
-		if err != nil {
-			return nil, err
-		}
 	}
 	h.initTracker.Store(path, true)
 	h.Unlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+
 
 	if !h.cacheEnabled {
 		//we don't have handle from the initialization, so open a new one.
@@ -135,41 +136,47 @@ func (h *fileHandleManager) getHandle(path string) (*os.File, error) {
 
 	//try to get it from the cache
 	var fhQ chan *os.File
-	var ok bool
+	var incache bool
 	var val interface{}
-	val, ok = h.fileHandlesQMap.Load(path)
-	if ok {
+
+	val, incache = h.fileHandlesQMap.Load(path)
+	if incache {
 		fhQ = val.(chan *os.File)
 		fh := <-fhQ
+		
 		return fh, nil
 	}
 
-	//at this point we need to create a new handle and create the pool handles if not full if it hasn't be created.
-	if fh, err = os.OpenFile(path, os.O_WRONLY, os.ModeAppend); err != nil {
-		return nil, err
-	}
-
-	//the pool is full return the handle
-	if h.cachedFileHandles+h.numOfHandlesPerFile > maxFileHandlesInCache {
-		util.PrintfIfDebug("getHandle -> Cache is full: %v\n", h.cachedFileHandles+h.numOfHandlesPerFile)
-		return fh, nil
-	}
-
-	//prime the pool with additional handles
-	if !isInit {
-		fhQ = make(chan *os.File, h.numOfHandlesPerFile)
-		h.cachedFileHandles = h.cachedFileHandles + 1
-		for i := 1; i < h.numOfHandlesPerFile; i++ {
-			var fhi *os.File
-			if fhi, err = os.OpenFile(path, os.O_WRONLY, os.ModeAppend); err != nil {
-				return nil, err
-			}
-			fhQ <- fhi
-			h.cachedFileHandles = h.cachedFileHandles + 1
+	//if init, the handle from the creation is not available (e.i. fh == nil)
+	if isInit {
+		if fh, err = os.OpenFile(path, os.O_WRONLY, os.ModeAppend); err != nil {
+			return nil, err
 		}
 
-		h.fileHandlesQMap.Store(path, fhQ)
 	}
+
+	h.Lock()
+	defer h.Unlock()
+	//ok not in the cache so we need to check if the cache is full
+	if h.cachedFileHandles+h.numOfHandlesPerFile > maxFileHandlesInCache {
+		return fh, nil
+	}
+
+	//add items to the cache
+	fhQ = make(chan *os.File, h.numOfHandlesPerFile)
+	h.cachedFileHandles = h.cachedFileHandles + 1
+	for i := 1; i < h.numOfHandlesPerFile; i++ {
+		var fhi *os.File
+
+		if fhi, err = os.OpenFile(path, os.O_WRONLY, os.ModeAppend); err != nil {
+			return nil, err
+		}
+		
+		fhQ <- fhi
+		h.cachedFileHandles = h.cachedFileHandles + 1
+	}
+
+	h.fileHandlesQMap.Store(path, fhQ)
 
 	return fh, nil
 
@@ -187,14 +194,18 @@ func (h *fileHandleManager) returnHandle(path string, fileHandle *os.File) error
 	var ok bool
 
 	val, ok = h.fileHandlesQMap.Load(path)
+
 	if ok {
 		fhq = val.(chan *os.File)
+		//fmt.Printf("return to dequeue %v\n", path)
 		fhq <- fileHandle
+		//fmt.Printf("after to dequeue %v\n", path)
 		h.fileHandlesQMap.Store(path, fhq)
 		return nil
 	}
 
 	//not found in the map, which is the case when the cache is at capacity
+	//fmt.Printf("close (not in the map) %v", path)
 	return fileHandle.Close()
 }
 
