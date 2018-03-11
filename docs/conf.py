f0ccb6385ef0a2c4422518abf030df2b08444cbd
Jesus Aguilar <3589801+giventocode@users.noreply.github.com>
2018-03-11 07:33:27 -0400

    -doc theme fix

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_bootstrap_theme
+#import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
